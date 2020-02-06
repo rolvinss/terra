2429270a8eb633a21050986d5ad3641eb695dfd9
Jaswant Singh <rolvinss@gmail.com>
2020-02-06 16:35:59 +0530

    daf

--- studentapp/network/subnets.tf
+++ studentapp/network/subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "subnets" {
-  count = data.aws_availability_zones.available.count
+  count = length(data.aws_availability_zones.available)
   cidr_block = cidrsubnet(var.VPC_CIDR,3 ,count.index )
   vpc_id = aws_vpc.main.id
 }
