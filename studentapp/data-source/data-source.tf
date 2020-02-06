0a9d46c0ba286014a6e142604bb035274dd7d262
Jaswant Singh <rolvinss@gmail.com>
2020-02-06 14:31:39 +0530

    daf

---
+++ studentapp/data-source/data-source.tf
@@ -0,0 +1,17 @@
+// TO fetch number of AZ's in the given region
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+output "AZS" {
+  value = data.aws_availability_zones.available.names
+}
+
+// To fetch the list of VPC's
+//
+//data "aws_vpcs" "vpcs" {}
+//
+//output "VPCS" {
+//  value = data.aws_vpcs.vpcs.ids
+//}
